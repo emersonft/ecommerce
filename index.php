81f2b7ee3bb56721f94272a16c4e6b6028e68068
emersonft <emersonft@hotmail.com>
2020-10-21 23:35:27 -0300

    Criamos a pageadmin

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -11,10 +12,19 @@ $app->config('debug', true);
 $app->get('/', function() {
   $page = new Page();
   
+  $page->setTpl("index");  
+});
+
+
+
+$app->get('/admin', function() {
+  $page = new PageAdmin();
+  
   $page->setTpl("index");
 
   
 });
+
 
 $app->run();
 
